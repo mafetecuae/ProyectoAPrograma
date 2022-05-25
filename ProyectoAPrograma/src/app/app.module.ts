286dc762c6a13898d836eb9e4577bb1d9098d5cd
Miguel fetecua <99681879+mafetecuae@users.noreply.github.com>
2022-05-24 23:00:43 -0500

    agregamos los components y las imagenes que usaremos

--- ProyectoAPrograma/src/app/app.module.ts
+++ ProyectoAPrograma/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutYouComponent } from './components/about-you/about-you.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/hard-and-soft-skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutYouComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    HardAndSoftSkillsComponent,
+    ProyectsComponent,
+    LogInComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
