d65dc8b9acb911b0441e4619338982060e18ff5b
Miguel fetecua <99681879+mafetecuae@users.noreply.github.com>
2022-05-27 00:26:52 -0300

    intentando usar los botones del proyecto

---
+++ ProyectoAPrograma/src/app.js
@@ -0,0 +1,9 @@
+var boton = document.getElementById('boton'),
+    overlay1 = document.getElementById('overlay1'),
+    formulario = document.getElementById('FormularioInicio'),
+    salir = document.getElementById('salir1');
+
+boton.addEventListener('click', function(){
+        overlay1.classList.add('active');
+});
+    
