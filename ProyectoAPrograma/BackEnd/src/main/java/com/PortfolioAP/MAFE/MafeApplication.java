8c6e4d993f1ebc4fd10582a40fd3556dbb368c59
Miguel fetecua <99681879+mafetecuae@users.noreply.github.com>
2022-06-05 13:52:26 -0300

    agregamos el spriing initializer
    agregamos el spring initializer en la carpeta backend

---
+++ ProyectoAPrograma/BackEnd/src/main/java/com/PortfolioAP/MAFE/MafeApplication.java
@@ -0,0 +1,13 @@
+package com.PortfolioAP.MAFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafeApplication.class, args);
+	}
+
+}
